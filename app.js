8953435f52e73f7061929c1ac7de2a0a7cb41bf7
Shane Murphy <MurphyShaneDev@gmail.com>
2025-04-08 14:38:06 +0000

    created function for drawing the snake character

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ const ctx = canvas.getContext("2d");
 
 const box = 20;
 const canvasSize = 400;
-let snake = [{ x: 160, y: 200 }];
+let snake = [{
+    x: 160,
+    y: 200
+}];
 let food = {
-  x: Math.floor(Math.random() * (canvasSize / box)) * box,
-  y: Math.floor(Math.random() * (canvasSize / box)) * box
+    x: Math.floor(Math.random() * (canvasSize / box)) * box,
+    y: Math.floor(Math.random() * (canvasSize / box)) * box
 };
 let direction = "RIGHT";
 let gameInterval;
+
+document.addEventListener("keydown", changeDirection);
+
+function changeDirection(e) {
+    if (e.key === "ArrowUp" && direction !== "DOWN") direction = "UP";
+    else if (e.key === "ArrowDown" && direction !== "UP") direction = "DOWN";
+    else if (e.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
+    else if (e.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw snake
+    for (let segment of snake) {
+        ctx.fillStyle = "#0f0";
+        ctx.fillRect(segment.x, segment.y, box, box);
+    }
+}
